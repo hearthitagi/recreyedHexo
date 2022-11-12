37568aa865ff39e085014d2643f2bbc2f6f8c5f2
recreyed <1760603940@qq.com>
2022-11-13 04:16:46 +0800

    随机背景图再加一张图

--- themes/butterfly/source/js/randombg.js
+++ themes/butterfly/source/js/randombg.js
@@ -1,5 +1,6 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
+    "url(https://f004.backblazeb2.com/file/recreyed/hexo/e15ba39e-95cc-4d48-8b85-ff7291dce776.jpeg)",
     "url(https://f004.backblazeb2.com/file/recreyed/hexo/63e5a771-8126-4e3d-a4fd-7c0ccafe0ab5.jpeg)",
     "url(https://f004.backblazeb2.com/file/recreyed/hexo/84bbd08f-3b2b-4afc-b52f-aad101b0128e.jpeg)",
     "url(https://f004.backblazeb2.com/file/recreyed/hexo/5e0596f2-3891-4a3f-a698-763e4f49f567.jpeg)",
