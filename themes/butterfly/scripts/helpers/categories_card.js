4e95380b07ead1d4a353b4d1ee07335f12fb1821
caijianxing <1760603940@qq.com>
2022-12-29 16:33:15 +0800

    自定义组件模板，分类磁贴

---
+++ themes/butterfly/scripts/helpers/categories_card.js
@@ -0,0 +1,11 @@
+/**
+ * Butterfly
+ * for aside categories
+ */
+
+'use strict'
+
+hexo.extend.helper.register('categories_card', function (categories, options) {
+  console.log(this.site.categories)
+  return `<div class="test"></div>`
+})
