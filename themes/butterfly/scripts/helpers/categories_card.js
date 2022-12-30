bd85774975d384776148df7a3600c4473fbdbd52
caijianxing <1760603940@qq.com>
2022-12-30 11:58:49 +0800

    分类磁贴修改

--- themes/butterfly/scripts/helpers/categories_card.js
+++ themes/butterfly/scripts/helpers/categories_card.js
@@ -1,11 +1,32 @@
 /**
  * Butterfly
- * for aside categories
+ * for categories card
  */
-
 'use strict'
-
 hexo.extend.helper.register('categories_card', function (categories, options) {
-  console.log(this.site.categories)
-  return `<div class="test"></div>`
+  // console.log(this.site.categories.data)
+  // console.log(hexo.theme.config.categoryBar.message)
+  const emojiList = ['🍁','🍀','🍂','🌸']
+  const generateList = () => {
+    let categoriesList = ''
+    this.site.categories.data.forEach((item,index) => {
+      for (const themeCategory of hexo.theme.config.categoryBar.message) {
+        if (item.name == themeCategory.title) {
+          item.cover = themeCategory.cover
+          item.descr = themeCategory.descr
+        }
+      }
+      categoriesList +=
+        `<div class="category-wrap" style="background: url(${item.cover}) center / cover">
+          <a class="category-link" href="${item.path}" data-pjax-state="">
+            <div class="category-title">| ${item.name} </div>
+            <div class="category-num">${emojiList[index]} ${item.length} </div>
+            <div class="category-descr">${item.descr} </div>
+          </a>
+        </div>`
+    });
+    return categoriesList
+  }
+  const list = generateList()
+  return `${list}`
 })
